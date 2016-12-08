213aac5fc969cc062ade1322ef9e77630c86eca6
changmiaomiao <940424841@qq.com>
2016-12-08 10:48:36 +0800

    完善

--- puzzle/js/index.js
+++ puzzle/js/index.js
@@ -1,79 +1,171 @@
-var fn={
-    ran:function ran(n, m) {
+var fn = {
+    ran: function ran(n, m) {
         return Math.round(Math.random() * (m - n) + n);
     },
-    on:function on(ele, type, fn) {
-        if (ele.addEventListener) {
-            ele.addEventListener(type, fn, false);
-            return;
-        }
-        if (!ele["aEvent" + type]) {
-            ele["aEvent" + type] = [];
-            ele.attachEvent("on" + type, function () {
-                run.call(ele);
-            });
-        }
-        var a = ele["aEvent" + type];
-        if (a && a.length) {
-            for (var i = 0, len = a.length; i < len; i++) {
-                if (a[i] == fn) {
-                    return
-                }
-            }
-            a.push(fn);
-        }
-
-    },
-    run:function run(e) {
-        e = e || window.event;
-        var type = e.type;
-        if (!e.target) {
-            e.target = e.srcElement;
-            e.pageX = (document.documentElement.scrollLeft || document.body.scrollLeft) + e.clientX;
-            e.pageY = (document.documentElement.scrollTop || document.body.scrollTop) + e.clientY;
-            e.stopPropagation = function () {
-                e.cancelBubble = true
-            };
-            e.preventDefault = function () {
-                e.returnValue = false;
-            }
-        }
-        var a = this["aEvent" + type];
-        if (a && a.length) {
-            for (var i = 0; i < a.length; i++) {
-                if (typeof a[i] == "function") {
-                    a[i].call(this, e);
-                } else {
-                    a.splice(i, 1);
-                    i--;
-                }
-            }
-        }
-    },
-    off:function off(ele, type, fn) {
-        if (ele.removeEventListener) {
-            ele.removeEventListener(type, fn, false);
-            return;
-        };
-        var a = ele["aEvent" + type];
-        if (a && a.length) {
-            for (var i = 0; i < a.length; i++) {
-                if (a[i] == fn) {
-                    a[i] = null;
-                    i--;
-                    break;
-                }
-            }
-        }
-    },
-    processThis:function processThis(fn, obj) {
+    processThis: function processThis(fn, obj) {
         return function (e) {
             fn.call(obj);
         }
     }
 };
+function BindEvent() {
+}
+BindEvent.prototype.on = function (ele, type, fn) {
+    if (ele.addEventListener) {
+        ele.addEventListener(type, fn, false);
+    }
+    if (!ele["aEvent" + type]) {
+        ele["aEvent" + type] = [];
+    }
+    var a = ele["aEvent" + type];
+    for (var i = 0, len = a.length; i < len; i++) {
+        if (a[i] == fn) {
+            return;
+        }
+    }
+    a.push(fn);
+
+};
+BindEvent.prototype.run = function (e, type) {
+    e = e || window.event;
+    type = type || e.type;
+    if (!e.target) {
+        e.target = e.srcElement;
+        e.pageX = (document.documentElement.scrollLeft || document.body.scrollLeft) + e.clientX;
+        e.pageY = (document.documentElement.scrollTop || document.body.scrollTop) + e.clientY;
+        e.stopPropagation = function () {
+            e.cancelBubble = true
+        };
+        e.preventDefault = function () {
+            e.returnValue = false;
+        }
+    }
+    var a = this.ele["aEvent" + type];
+    if (a && a.length) {
+        for (var i = 0; i < a.length; i++) {
+            if (typeof a[i] == "function") {
+                a[i].call(this, e);
+            } else {
+                a.splice(i, 1);
+                i--;
+            }
+        }
+    }
+};
+BindEvent.prototype.off = function (ele, type, fn) {
+    if (ele.removeEventListener) {
+        ele.removeEventListener(type, fn, false);
+    }
+    ;
+    var a = ele["aEvent" + type];
+    if (a && a.length) {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] == fn) {
+                a[i] = null;
+                i--;
+                break;
+            }
+        }
+    }
+};
+function Drag(ele) {
+    this.mx = this.my = this.y = this.x = null;
+    this.ele = ele;
+    this.DOWN = fn.processThis(this.down, this);//this上的down;
+    this.MOVE = fn.processThis(this.move, this);
+    this.UP = fn.processThis(this.up, this);
+    this.on(this.ele, "mousedown", this.DOWN);
+};
+Drag.prototype.__proto__ = BindEvent.prototype;
+Drag.prototype.down = function (e) {
+    e = e || window.event;
+    this.x = this.ele.offsetLeft;
+    this.y = this.ele.offsetTop;
+    this.mx = e.pageX;
+    this.my = e.pageY;
+    this.l = this.x;
+    this.t = this.y;
+    this.ele.style.zIndex = 10;
+    if (this.ele.setCapture) {
+        this.ele.setCapture();
+        this.on(this.ele, "mousemove", this.MOVE);
+        this.on(this.ele, "mouseup", this.UP);
+    } else {
+        this.on(document, "mousemove", this.MOVE);
+        this.on(document, "mouseup", this.UP);
+    }
+    e.preventDefault();
+    this.run(e, "star");
+    this.on(this.ele, "isHit", this.isHit);
+};
+Drag.prototype.move = function (e) {
+    e = e || window.event;
+    this.left = this.x + (e.pageX - this.mx);
+    this.top = this.y + (e.pageY - this.my);
+    this.ele.style.left = this.x + (e.pageX - this.mx) + "px";
+    this.ele.style.top = this.y + (e.pageY - this.my) + "px";
+    //this.run(e,"moving");
+    this.run(e, "resetMove");
+};
+Drag.prototype.up = function (e) {
+    this.ele.style.zIndex = 0;
+    if (this.ele.releaseCapture) {
+        this.ele.releaseCapture();
+        this.off(this.ele, "mousemove", this.MOVE);
+        this.off(this.ele, "mouseup", this.UP);
+    } else {
+        this.off(document, "mousemove", this.MOVE);
+        this.off(document, "mouseup", this.UP);
+    }
+    this.run(e, "end");
+    this.run(e, "isHit");
+};
+Drag.prototype.range = function (oRange) {
+    this.oRange = oRange;
+    this.on(this.ele, "resetMove", this.addRange);
+};
+Drag.prototype.addRange = function (e) {
+    var oRange = this.oRange;
+    var valX = this.left;
+    var valY = this.top;
+    if (valX >= oRange.right) {
+        valX = oRange.right;
+    } else if (valX <= oRange.left) {
+        valX = oRange.left;
+    }
+    this.ele.style.left = valX + "px";
+    if (valY >= oRange.bottom) {
+        valY = oRange.bottom;
+    } else if (valY <= oRange.top) {
+        valY = oRange.top;
+    }
+    this.ele.style.top = valY + "px";
+};
+Drag.prototype.isHit=function (){
+    var pos=document.getElementById("pos");
+    pos.posLeft=pos.offsetLeft;
+    pos.posTop=pos.offsetTop;
+
+    var _true=(this.x+this.ele.offsetWidth==pos.posLeft&&this.y==pos.offsetTop) || (this.y+this.ele.offsetHeight==pos.offsetTop&&this.x==pos.offsetLeft) || (this.x-this.ele.offsetWidth==pos.posLeft&&this.y==pos.offsetTop) || (this.y-this.ele.offsetHeight==pos.offsetTop&&this.x==pos.posLeft)
+        ;
+    if(_true){
+        this.ele.style.left=pos.posLeft+"px";
+        this.ele.style.top=pos.posTop+"px";
+        pos.style.left=this.x+"px";
+        pos.style.top=this.y+"px";
+    }
+    else{
+        this.ele.style.left=this.x+"px";
+        this.ele.style.top=this.y+"px";
+    };
+};
 var render = (function () {
     var conImg = document.getElementById("conImg");
+    var sample = document.getElementById("sample");
+    var sampleLeft = sample.offsetLeft;
+    var sampleTop = sample.offsetTop;
+    var sampleRight = sample.clientWidth;
+    var sampleBottom = sample.clientHeight;
     var spans = "";
     var arr = [];
     function numData(n) {
@@ -83,95 +175,32 @@ var render = (function () {
             var boo = arr.filter(function (index) {
                 return index == num;
             });
-            //console.log(boo,arr);
             if (boo.length == 0) {
                 arr.push(num);
             }
             i = arr.length;
         }
     };
-    function inn(n){
+    function inn(n) {
         n = n ? 9 : n;
         numData(n);
         for (var i = 0; i < n - 1; i++) {
             spans += "<span class='img" + arr[i] + "'></span>";
         }
+        spans+="<span id='pos'></span>";
         conImg.innerHTML = spans;
         var spansClass = document.getElementsByTagName("span");
         for (i = n - 2; i >= 0; i--) {
             var span = spansClass[i];
             span.className += " pos" + i;
-            fn.on(span, "mousedown", down);
+            var spanOn = new Drag(span);
+            spanOn.on(span, "start");
+            spanOn.range({left: 0, top: 0, right: sampleRight-span.offsetWidth, bottom: sampleBottom - span.offsetHeight});
+            spanOn.n=n;
         }
         spans = "";
         arr = [];
     };
-    function down(e) {
-        e = e || window.event;
-        this.x = this.offsetLeft;
-        this.y = this.offsetTop;
-        this.mx = e.pageX;
-        this.my = e.pageY;
-        this.l = this.x;
-        this.t = this.y;
-        console.log(this.x,this.y);
-        this.style.zIndex=10;
-        if (this.setCapture) {
-            this.setCapture();
-            this.MOVE = move.bind(this);
-            this.UP = up.bind(this);
-            fn.on(document, "mousemove", this.MOVE);
-            fn.on(document, "mouseup", this.UP);
-        } else {
-            fn.on(this, "mousemove", move);
-            fn.on(this, "mouseup", up);
-        }
-        e.preventDefault();
-    }
-    function move(e) {
-        e = e || window.event;
-        this.left=this.x + (e.pageX - this.mx);
-        this.top=this.y + (e.pageY - this.my);
-        if(this.left==0&&this.left==480){
-            console.log("1");
-        }else if(this.top==0&&this.left==340){
-            console.log("2");
-        }
-        this.style.left = this.x + (e.pageX - this.mx) + "px";
-        this.style.top = this.y + (e.pageY - this.my) + "px";
-    }
-    function up() {
-        this.style.zIndex=0;
-        if (this.releaseCapture) {
-            this.releaseCapture();
-            fn.off(this, "mousemove", this.MOVE);
-            fn.off(this, "mouseup", this.UP);
-        } else {
-            fn.off(this, "mousemove", move);
-            fn.off(this, "mouseup", up);
-        }
-            this.left-=this.x;
-            this.top-=this.y;
-            if(this.left>100){
-                this.style.left=this.l+240+"px";
-                this.style.top =this.t+"px";
-            }else if(this.left<-100){
-                this.style.left=this.l-240+"px";
-                this.style.top =this.t+"px";
-            }else{
-                if(this.top>100){
-                    this.style.top=this.t+170+"px";
-                    this.style.left =this.l+"px";
-                }else if(this.top<-100){
-                    this.style.top=this.t-170+"px";
-                    this.style.left =this.l+"px";
-                }else{
-                    this.style.left =this.l+"px";
-                    this.style.top=this.t+"px";
-                }
-            }
-
-    }
     return {
         init: function () {
             var begin = document.getElementById("begin");
@@ -179,7 +208,9 @@ var render = (function () {
                 window.clearTimeout(time);
                 inn(9);
             };
-            var time=window.setTimeout(function(){inn(9)},1000);
+            var time = window.setTimeout(function () {
+                inn(9)
+            }, 1000);
             //inn(9);
         }
     }
